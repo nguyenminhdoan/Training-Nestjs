29fd411be226e78b1b07788611d7d33eca6e06a8
nguyenminhdoan <50704169+nguyenminhdoan@users.noreply.github.com>
2021-08-13 17:58:18 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   app.enableCors();
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('hello')
+    .setDescription('Nest API project training')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(config.get('PORT'));
 }
 bootstrap();
